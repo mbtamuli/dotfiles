862a519768f843679a043be6586aeb96e9c3d4e2
Mriyam Tamuli <mbtamuli@gmail.com>
2020-09-03 23:00:15 +0530

    Add fish config
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+set --export --global GOPATH "$HOME/workspace/go"
+set --export --global LC_ALL "en_IN.UTF-8"
+set --export --global LANG "en_IN.UTF-8"
+set --export --global LANGUAGE "en_IN.UTF-8"
+set --export --global EDITOR vim
+set --export --global GPG_TTY (tty)
