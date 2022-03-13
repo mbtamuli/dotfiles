178f5bd4e57a006d7088d607280739df1275df88
Mriyam Tamuli <mbtamuli@gmail.com>
2022-03-13 15:53:16 +0530

    :sparkle: NEW: Add macOS setup script
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

---
+++ .local/share/bin/setup_macos.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+formulae=(
+	bat
+	pass
+	kind
+	golangci-lint
+	protoc-gen-go
+	coreutils
+	zsh
+	kube-ps1
+	gh
+	fzf
+)
+casks=(
+	docker
+	google-chrome
+	rectangle
+	visual-studio-code
+	iterm2
+	multipass
+	keepingyouawake
+	slack
+)
+
+function install_formula() {
+	brew install $1
+}
+
+function install_cask() {
+	brew install --cask $1
+}
+
+function install_formulae() {
+	for formula in "${formulae[@]}"; do
+		install_formula $formula
+	done
+}
+
+function install_casks() {
+	for cask in "${casks[@]}"; do
+		install_cask $cask
+	done
+}
+
+function main() {
+	install_formulae
+	install_casks
+}
+
+main
